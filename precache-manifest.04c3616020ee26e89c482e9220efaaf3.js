435f41dba3b406d4ca14bf2307633a18295d99c0
Your Name <muhdsyahrulnizam123@gmail.com>
2019-08-25 09:30:11 +0800

    deploy

---
+++ precache-manifest.04c3616020ee26e89c482e9220efaaf3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "f83465070f9a48c6728e6a74417141b0",
+    "url": "/WorldResume/fonts/materialdesignicons-webfont.f8346507.ttf"
+  },
+  {
+    "revision": "c10b0dcd6981ae16bc72cf1d6c32a8da",
+    "url": "/WorldResume/three.r92.min.js"
+  },
+  {
+    "revision": "2da4365b73fdf6e07b8f6f9acaf0d107",
+    "url": "/WorldResume/vanta.net.min.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/WorldResume/robots.txt"
+  },
+  {
+    "revision": "ae619c7b719975e63add",
+    "url": "/WorldResume/js/chunk-vendors.1e54f4d0.js"
+  },
+  {
+    "revision": "1702b785e122a41d7121",
+    "url": "/WorldResume/js/app.b36d3f5f.js"
+  },
+  {
+    "revision": "16180c8461be2b16ef93a95664c2af6c",
+    "url": "/WorldResume/index.html"
+  },
+  {
+    "revision": "a0103549b5dca7100830a06fc1a09cb9",
+    "url": "/WorldResume/img/metamask.a0103549.svg"
+  },
+  {
+    "revision": "a8ad87308f823309518e69dc62b8f060",
+    "url": "/WorldResume/fonts/materialdesignicons-webfont.a8ad8730.woff2"
+  },
+  {
+    "revision": "9262ae17dbf8ff24e193f88c767d6c4f",
+    "url": "/WorldResume/fonts/materialdesignicons-webfont.9262ae17.eot"
+  },
+  {
+    "revision": "31174c6f35fcbd9772533e2bf9241c58",
+    "url": "/WorldResume/fonts/materialdesignicons-webfont.31174c6f.woff"
+  },
+  {
+    "revision": "ae619c7b719975e63add",
+    "url": "/WorldResume/css/chunk-vendors.4d19a465.css"
+  },
+  {
+    "revision": "1702b785e122a41d7121",
+    "url": "/WorldResume/css/app.45e4f4a5.css"
+  }
+];
